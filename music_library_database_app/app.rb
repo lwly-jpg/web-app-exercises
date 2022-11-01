bf3c48a84c1a42b65f91bd8155d55108efc2dc9b
adonl <adnlwly@gmail.com>
2022-11-01 10:01:49 +0000

    clean up app file

--- music_library_database_app/app.rb
+++ music_library_database_app/app.rb
@@ -28,8 +28,7 @@ class Application < Sinatra::Base
   get '/albums/:id' do
     repo = AlbumRepository.new
     artist_repo = ArtistRepository.new
-    p params[:id].to_i
-    @album = repo.find(params[:id].to_i)
+    @album = repo.find(params[:id])
     @artist = artist_repo.find(@album.artist_id)
     
     return erb(:album)
